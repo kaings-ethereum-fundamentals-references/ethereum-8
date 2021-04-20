8f540dc9edf0dc1991c4b838ded118dc5533fed4
willys_tjiawi <willys@huubap.com>
2021-04-20 23:27:05 +0800

    added contract frontend

---
+++ src/app.js
@@ -0,0 +1,19 @@
+App = {
+    web3Provider: null,
+    contracts: {},
+    account: '0x0',
+
+    init: () => App.initWeb3(),
+    initWeb3: () => {
+
+    },
+    initContract: () => {
+
+    },
+    placeBet: () => {
+
+    },
+    render: () => {
+        
+    }
+}
